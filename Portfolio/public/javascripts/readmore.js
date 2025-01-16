5b544a5dd9603bf6ee57fe9153df8722e72d3a40
Alex Purser <AP276620@falmouth.ac.uk>
2025-01-16 22:26:33 +0000

    Added project details page styling and functionality

--- Portfolio/public/javascripts/readmore.js
+++ Portfolio/public/javascripts/readmore.js
@@ -1,33 +1,12 @@
 
-// Make the popup visible and get which button was clicked
-function ReadMore(index) {
-
-    
-    // finding the correct popup
-    var popup_box = document.getElementById('popup_box' + index);
-    var page_content = document.getElementById('page_content');
-
-    page_content.classList.add('blur');
-    popup_box.classList.add('popup_box_active');
-
-
-    // setup the starting slide
-    showSlide(index, 0);
-}
-
-
-// disable the popup
-function CloseMore(index) {
-    var popup_box = document.getElementById('popup_box' + index);
-    popup_box.classList.remove('popup_box_active');
-
-    var page_content = document.getElementById('page_content');
-    page_content.classList.remove('blur');
-}
+// when window loads, show first slide
+window.onload = function() {
+    showSlide(0);
+};
 
 
 // show slide in the carousel
-function showSlide(index, pic_index) {
+function showSlide(pic_index) {
 
     // Disable already active slide and dot
     var active_slide = document.querySelector('.active_popup_pic');
@@ -38,11 +17,11 @@ function showSlide(index, pic_index) {
     }
 
     // Enable selected slide
-    var selected_slide = document.getElementById('slide' + index + pic_index);
+    var selected_slide = document.getElementById('slide' + pic_index);
     selected_slide.classList.add('active_popup_pic'); 
 
     
     // Enable selected dot
-    var selected_dot = document.getElementById('slide_dot' + index + pic_index)
+    var selected_dot = document.getElementById('slide_dot'+ pic_index)
     selected_dot.classList.add('active_dot');
 }

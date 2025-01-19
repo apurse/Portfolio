f9b7e2fb36c34d95bc82a0656375e0e6588a9250
Alex Purser <AP276620@falmouth.ac.uk>
2025-01-19 16:38:37 +0000

    Fixed bulb line animation, straightened icons

--- Portfolio/public/javascripts/burgerMenu.js
+++ Portfolio/public/javascripts/burgerMenu.js
@@ -1,16 +1,13 @@
 
 // when burger button is clicked
 function burgerToggle() {
-    var burger_button = document.getElementById('burger_button');
     var burger_contents = document.getElementById('burger_contents');
-    // burger_button.classList.toggle('burger_active');
     burger_contents.classList.toggle('burger_active');
 
     // if active, get height
     if (burger_contents.classList.contains('burger_active')) {
         burger_contents.style.maxHeight = burger_contents.scrollHeight + "px";
         burger_contents.style.display = 'block';
-
     } else {
         burger_contents.style.maxHeight = "0";
     }
